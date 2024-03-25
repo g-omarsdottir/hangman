aef924e9d4a96e07382032713f91e15566e2306b
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-03-25 10:17:13 +0000

    Add welcome message for start screen

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome():
+    """
+    Function that welcomes the user to the game and explains the basic rules.
+    """
+    print("Welcome! Testing")
+
+    print(
+    """
+    Welcome to a game of Hangman!
+        
+    You will get a set of blanks representing the number of letters in a word.
+    Guess the word by entering one letter at a time and press enter.
+            
+    If you guess correctly, your letter(s) will appear on the blank(s).
+            
+    With each incorrect guess, one element will be added to the stick figure
+    on the gallows, so choose wisely. 
+            
+    After 6 incorrect guesses, the stick figure is complete and it’s game over.
+            
+    Let the games begin!
+            
+    """
+    )
+
+
+def 
