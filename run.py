bda1bcc8269c633d94c1ebcc6332e00f8d46eb70
Gudrun Omarsdottir <g.omarsdottir@gmail.com>
2024-03-28 18:05:23 +0000

    Fix chronology of calling functions for game logic
    - Add to function prompting user’s guess: call function word_puzzle to display blanks to indicate number of blanks before prompting for guess
    - Add list used_letters as argument to functions handling user’s guess to make list of used letters reachable
    - Fix main function: pass used_letters as argument for compare_guess instead of redefining as variable
    - Remove excess parentheses in prompt for username

--- run.py
+++ run.py
@@ -134,7 +134,7 @@ def choice_username():
     Passes the username input for validation to the validate_username function.
     """
     while True:
-        username = (input("Choose your username: ").strip())
+        username = input("Choose your username: ").strip()
         if validate_username(username):
             return username
 
@@ -161,14 +161,15 @@ def get_guess():
     Passes the letter for validation to the validate_guess function.
     """
     while True:
+        word_puzzle(used_letters)
         guess = input("Guess a letter: ").strip().upper()
-        if validate_guess(guess):
-            print("function: get_guess")
+        if validate_guess(guess, used_letters):
+            print("rendering function: get_guess")
             break
     return guess
 
 
-def validate_guess(guess):
+def validate_guess(guess, used_letters):
     """
     Function that validates user input to ensure it is a single letter.
     Returns the validated letter or False if input is invalid.
@@ -179,31 +180,30 @@ def validate_guess(guess):
     elif len(guess) != 1:
         print("Enter a single letter.")
         return False
-    # Add validation for used letters
     else:
         print(f"Let's see if {guess} works...")
-        return compare_guess(guess, word, guesses_left)
+        return compare_guess(guess, word, guesses_left, used_letters)
 
 
-def compare_guess(guess, word, guesses_left):
+def compare_guess(guess, word, guesses_left, used_letters):
     """
     Function that compares user's guess with the word to guess and already used letters.
     Returns the correct guess or appends the wrong guess to used_letters list.
     """
     while guesses_left > 0:
-        if guess in used_letters:                               # Doesn't work. If wrong letter guessed, game stops.
+        if guess in used_letters:                               
             print(f"You've already guessed {guess}. Try again.")
             return
-        elif guess not in word:                                 # Doesn't work
+        elif guess not in word:                                 
             used_letters.append(guess)
             guesses_left -= 1
             print(f"Wrong guess, {guess} is not correct.")
-            return " ".join([letter if letter in used_letters else "_" for letter in word])
-            return guesses_left
-        else:                                                   
-            print(f"Great job, {guess} is correct!")            # Works: the correct guess (letter) is printed
-            used_letters.append(guess)                          # Doesn't work
-            return " ".join([letter if letter in used_letters else "_" for letter in word]) # Doesn't work
+            used_letters = " ".join([letter if letter in used_letters else "_" for letter in word])
+            return guesses_left, used_letters
+        else:
+            print(f"Great job, {guess} is correct!")            
+            used_letters.append(guess)                          
+            return " ".join([letter if letter in used_letters else "_" for letter in word]) 
 
 
 def word_puzzle(used_letters):
@@ -218,7 +218,7 @@ def word_puzzle(used_letters):
         print(hangman_drawing[guesses_left])
         return True
     if guesses_left > 0 and "_" not in blanks:
-        print("Congratulations, {username}, you won!") 
+        print("Congratulations, {username}, you won!")
     if guesses_left == 0 and "_" in blanks:
         print("Too bad, {}, you lost.")
         return choice_play_again()
@@ -241,7 +241,7 @@ def choice_play_again():
             return choice_play_game()
         else:
             print("Invalid input. Please enter 'y' or 'n'.")
-            return False         
+            return False
 
 
 def main():
@@ -253,8 +253,7 @@ def main():
     choice_username()
     
     guess = get_guess()
-    compare_guess(guess, word, guesses_left)
-    used_letters = compare_guess(guess, word, guesses_left)
+    compare_guess(guess, word, guesses_left, used_letters)
     word_puzzle(used_letters)
     
 
